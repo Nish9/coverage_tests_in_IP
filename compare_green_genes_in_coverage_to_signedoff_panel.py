5ff7c279383caf6d96656b8a214149faf10b0a14
Nish9 <t.nishi.89@gmail.com>
2020-11-04 00:33:36 +0000

    Update compare_green_genes_in_coverage_to_signedoff_panel.py
    Correcting some mistakes in the code so comparing coveragge data between the portal and panelapp data works accurately.

--- compare_green_genes_in_coverage_to_signedoff_panel.py
+++ compare_green_genes_in_coverage_to_signedoff_panel.py
@@ -22,7 +22,9 @@ def get_coverage_data(interpretation_request):
     panels_genecount = []
     coverage_json = interpretation_request['genePanelsCoverage']
     for panel in coverage_json:
-        panels_genecount.append({'panelId':panel,'genecount':len(coverage_json[panel])})
+        if "SUMMARY" in coverage_json[panel].keys():
+            coverage_json[panel].pop("SUMMARY")
+        panels_genecount.append({'panelId':panel,'green_gene_count':(len(coverage_json[panel]))})
     return panels_genecount
 
 
@@ -35,17 +37,19 @@ def signedoff_green_genecount(paneldata):
         for gene in signedoffpanel['genes']:
             if gene["confidence_level"] == "3":
                 panelapp_green_gene_list.append(gene['gene_data']["gene_symbol"])
-        panelapp_green_genecounts.append({'panelID':panel['panelId'],'green_gene_count': len(set(panelapp_green_gene_list))})
+        panelapp_green_genecounts.append({'panelId':panel['panelId'],'green_gene_count': len(set(panelapp_green_gene_list))})
     return panelapp_green_genecounts
 
 for case in caselist:
     IR = getIR(case)
     paneldata = get_coverage_data(IR)
+    print("paneldata:", paneldata)
     panelappRefData = signedoff_green_genecount(paneldata)
+    print("panelappdata:", panelappRefData)
     pairs = zip(paneldata, panelappRefData)
     notequal = [(x, y) for x, y in pairs if x != y]
     if not notequal:
         print("Gene count in coverage data matches the green gene count in signed off panels")
     else:
         for item in notequal:
-            print("Panel with id {} has {} genes in coverage data, while the number of green genes in the gms signed off panel is {}".format(item[0]['panelId'], item[0]['genecount'], item[1]['green_gene_count']))
+            print("Panel with id {} has {} genes in coverage data, while the number of green genes in the gms signed off panel is {}".format(item[0]['panelId'], item[0]['green_gene_count'], item[1]['green_gene_count']))
